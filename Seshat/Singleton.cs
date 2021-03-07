599a20d9a82ef79ce8c25c26ffddda6c6b7fec45
Dante <frostu8@protonmail.com>
2021-03-07 15:08:33 -0500

    use internally defined singleton type

---
+++ Seshat/Singleton.cs
@@ -0,0 +1,21 @@
+﻿namespace Seshat
+{
+    /// <summary>
+    /// Helper class for singleton types, like registrars.
+    /// </summary>
+    /// <typeparam name="T">A class to make a singleton.</typeparam>
+    public class Singleton<T> where T: class, new()
+    {
+        private static T _instance;
+
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new T();
+                return _instance;
+            }
+        }
+    }
+}
