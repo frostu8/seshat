932c620aec07295dec540226dcf45fa48adb02a7
Dante <frostu8@protonmail.com>
2021-03-07 17:19:25 -0500

    improve monomod patching

--- Seshat/Patches/ItemXmlDataList.cs
+++ Seshat/Patches/ItemXmlDataList.cs
@@ -5,8 +5,21 @@ using MonoMod;
 using Seshat.API;
 
 [MonoModPatch("ItemXmlDataList")]
+[MonoModNoNew]
 public class ItemXmlDataList
 {
+    // disable unused field warnings, we know what we're doing!
+#pragma warning disable CS0169, IDE0051, IDE0044
+    [MonoModRemove]
+	private List<DiceCardXmlInfo> _cardInfoList;
+
+	[MonoModRemove]
+	private List<DiceCardXmlInfo> _basicCardList;
+
+	[MonoModRemove]
+	private Dictionary<int, DiceCardXmlInfo> _cardInfoTable;
+#pragma warning restore CS0169, IDE0051, IDE0044
+
     [MonoModReplace]
     public void InitCardInfo_V2(List<DiceCardXmlInfo> list)
     {
