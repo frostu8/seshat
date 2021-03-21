a0e65e4d5120fa4f439b8fba500d749a34039d5f
Dante <frostu8@protonmail.com>
2021-03-20 23:28:09 -0400

    preformance patches for UIInvenCardList
    This function is absolutely horrid, but with the power of MonoMod we can
    patch our troubles away.

---
+++ Seshat/Patches/UI/UIInvenCardList.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using MonoMod;
+
+using AbilityDesc = BattleCardAbilityDescXmlList;
+
+namespace UI
+{
+    public class patch_UIInvenCardList : UIInvenCardList
+    {
+        // WOAH NELLY! THIS FUNCTION IS SUPER OVERCOMPLICATED! Lets LINQ it up!
+        [MonoModReplace]
+        public new List<DiceCardItemModel> GetCardsByDetailFilterUI(List<DiceCardItemModel> cards)
+        {
+            // get filters
+            UICardListDetailFilterPopup filter = CardFilter.GetDetailFilter();
+            var rarityFilter = filter.CheckRarityDetailFilter();
+            var diceFilter = filter.CheckDiceDetailFilter();
+            var bufFilter = filter.CheckBufDetailFilter();
+            var abilityFilter = filter.CheckAbilityDetailFilter();
+            var diceCountFilter = filter.CheckDiceCountDetailFilter();
+
+            return cards
+                .Where(card => rarityFilter.Count <= 0 ||
+                    rarityFilter.Contains(card.GetRarity().ToString()))
+                .Where(card => diceFilter.Count <= 0 ||
+                    card.GetBehaviourList().Any(dice => diceFilter.Contains(dice.Detail.ToString()) || diceFilter.Contains(dice.Type.ToString())))
+                .Where(card => bufFilter.Count <= 0 ||
+                    AbilityDesc.Instance.GetAbilityKeywords(card.ClassInfo.Script).Any(k => bufFilter.Contains(k)) ||
+                    card.GetBehaviourList().Any(dice => AbilityDesc.Instance.GetAbilityKeywords(dice.Script).Any(k => bufFilter.Contains(k))))
+                .Where(card => abilityFilter.Count <= 0 ||
+                    AbilityDesc.Instance.GetAbilityKeywords(card.ClassInfo.Script).Any(k => abilityFilter.Contains(k)) ||
+                    card.GetBehaviourList().Any(dice => AbilityDesc.Instance.GetAbilityKeywords(dice.Script).Any(k => abilityFilter.Contains(k))))
+                .Where(card => diceCountFilter.Count <= 0 ||
+                    diceCountFilter.Contains(card.GetBehaviourList().Count))
+                .ToList();
+        }
+    }
+}
