ef4130453cccfe9a43ae13edf454c8159f84a646
Dante <frostu8@protonmail.com>
2021-03-20 20:42:14 -0400

    made sixty nine g'wood

--- Seshat.ExampleMod/SixtyNine.cs
+++ Seshat.ExampleMod/SixtyNine.cs
@@ -1,14 +1,23 @@
-﻿using Seshat.Attribute;
+﻿using System;
+using Seshat.Attribute;
 
 namespace Seshat.ExampleMod
 {
     [DiceAbility("sixty_nine")]
     public class SixtyNine : DiceCardAbilityBase
     {
+        public override void OnRollDice()
+        {
+            base.behavior.ApplyDiceStatBonus(new DiceStatBonus
+            {
+                dmgRate = -9999,
+                breakRate = -9999
+            });
+        }
+
         public override void OnSucceedAttack()
         {
-            //base.card.target.TakeDamage((int)base.card.target.hp, DamageType.Card_Ability, base.owner);
-            base.owner.bufListDetail.AddKeywordBufByCard(KeywordBuf.Strength, 143, base.owner);
+            base.card.target.TakeDamage((int)Math.Ceiling(base.card.target.hp), DamageType.Card_Ability, base.owner);
         }
     }
 }
